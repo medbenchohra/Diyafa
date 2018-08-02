d7c73cdf8be93ae708649fb16827d5ad114695d3
medbenchohra <medbenchohra96@gmail.com>
2018-08-02 23:39:24 +0300

    d

---
+++ app/src/main/java/dz/diyafa/diyafa/User.java
@@ -0,0 +1,20 @@
+package dz.diyafa.diyafa;
+
+public class User {
+    private String userName;
+    private String password;
+    private String email;
+    private String country;
+    private String region;
+    private String phone;
+
+
+    public User(String userName,String password){
+        this.userName=userName;
+        this.password=password;
+    }
+
+
+
+
+}
